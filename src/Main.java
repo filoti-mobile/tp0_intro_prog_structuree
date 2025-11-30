c69c9cf901108e9769bf64b0c0e76ed9ca7df972
toscanoGithub <pasarela70@hotmail.com>
2025-11-30 15:32:47 -0500

    premier programme

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import static java.lang.IO.print;
+import static java.lang.IO.println;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    println("Bienvenue,");
+    print("Naviguez dans le dossier '_exercices' pour voir mon travail");
+}
