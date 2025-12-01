5234bde71c3cf1f62768e051becce8c4ea9d164c
toscanoGithub <pasarela70@hotmail.com>
2025-11-30 19:24:38 -0500

    exercice 25 Valeur maximale

---
+++ src/_exercices/ex_25.java
@@ -0,0 +1,19 @@
+import static java.lang.IO.print;
+import static java.lang.IO.println;
+
+void main() {
+    /* Écrivez un programme qui demande trois valeurs réelles et qui affiche leur valeur maximale.*/
+
+    Scanner scanner = new Scanner(System.in);
+    print("Entrez trois valeurs :");
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    int c = scanner.nextInt();
+
+    double max = Math.max(Math.max(a, b), c);
+    print("La valeur maximale est " + max);
+
+
+
+
+}
