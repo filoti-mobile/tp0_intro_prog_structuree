c6663bcdbc996ced933afd2a626e261322a14445
toscanoGithub <pasarela70@hotmail.com>
2025-11-30 19:25:30 -0500

    exercice 25 Valeur maximale

--- src/_exercices/ex_25.java
+++ src/_exercices/ex_25.java
@@ -11,7 +11,7 @@ void main() {
     int c = scanner.nextInt();
 
     double max = Math.max(Math.max(a, b), c);
-    print("La valeur maximale est " + max);
+    println("La valeur maximale est " + max);
 
 
 
